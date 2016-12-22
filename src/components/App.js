358538511b7c3207fe7513af4725149ebdde4bf2
Roger <rogerholmgren@gmail.com>
2016-12-22 16:30:49 +0100

    added aap.js view template

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,12 @@
 // This is the template used by every page.
 import React, {PropTypes} from 'react';
+import Header from './common/Header';
 
 class App extends React.Component {
   render() {
     return (
       <div className="container-fluid">
-        <p>Header here...</p>
+        <Header/>
         {this.props.children}
       </div>
     );
